cd554e790b2fdbed231907ab01cf9c982823de46
Raphael Martinez <70238369+raphamartinez@users.noreply.github.com>
2022-01-21 20:58:07 -0300

    props test

--- pages/tempo.js
+++ pages/tempo.js
@@ -1,12 +1,25 @@
-function Tempo() {
-    const date = new Date();
-    const dateString = date.toGMTString();
+function Tempo(props) {
+    const dynamicDate = new Date();
+    const dynamicDateString = dynamicDate.toGMTString();
 
     return(
         <div>
-            {dateString} (dinâmico)
+            <div>{dynamicDateString} (dinâmico)</div>
+            <div>{props.staticDateString} (estatico)</div>
         </div>
     )
 }
+
+export function getStaticProps(){
+    const staticDate = new Date();
+    const staticDateString = staticDate.toGMTString();
+
+    return{
+        props: {
+            staticDateString
+        }
+    }
+}
+
 
 export default Tempo;
