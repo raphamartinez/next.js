0e0f1bcf19a95f0a776ed72835d9d6442ec54dd6
Raphael Martinez <70238369+raphamartinez@users.noreply.github.com>
2022-01-21 20:41:06 -0300

    second test

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,14 @@
-import { useState } from 'react';
+import Link from 'next/link';
 
 function Home() {
     return (
         <div>
-            <h2>Home 4</h2>
-            <Contador/>
-            <div>Teste</div>
+            <h1>Home</h1>
+            <Link href="/sobre">
+                <a>Acessar Sobre </a>
+            </Link>
         </div>
     )
 };
 
-function Contador() {
-    const [contador, setContador] = useState(1);
-
-    function adicionarContador() {
-        setContador(contador + 1);
-    };
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    );
-};
 export default Home;
